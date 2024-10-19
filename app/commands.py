0fe4801a96270e1fa663164ee7afc2ab8ffd2266
himetik <himetik@tutamail.com>
2024-10-19 21:49:17 +0300

    Add sentence validation in cli interface

--- app/commands.py
+++ app/commands.py
@@ -3,6 +3,7 @@
 
 import click
 from app.sentence_extractor import get_sentence_by_id, get_random_sentence, get_random_sentence_by_word
+from app.sentence_validator import validate_sentence, SentenceValidationError
 
 
 @click.command()
@@ -32,3 +33,21 @@ def word(word):
         click.echo(sentence)
     else:
         click.echo(f"Sentence containing word '{word}' not found")
+
+
+@click.command()
+def put():
+    while True:
+        statement = click.prompt("Enter a statement to validate (or '\quit' to exit)")
+
+        if statement.lower() == '\quit':
+            click.echo("Exiting the validation process.")
+            break
+
+        try:
+            validate_sentence(statement)
+            click.echo(f'"{statement}" is valid.')
+        except SentenceValidationError as e:
+            click.echo(f"Error: {e}")
+
+        click.echo()
--- app/commands_registrator.py
+++ app/commands_registrator.py
@@ -1,6 +1,6 @@
 import click
 from typing import Any
-from .commands import num, random, word
+from .commands import num, random, word, put
 
 
 @click.group()
@@ -9,7 +9,7 @@ def cli() -> None:
 
 
 def register_commands() -> None:
-    commands = [num, random, word]
+    commands = [num, random, word, put]
     for command in commands:
         cli.add_command(command)
 
