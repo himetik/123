d56186948c10cb77543f0b19ae573b5b451d3677
himetik <himetik@tutamail.com>
2024-10-19 21:05:05 +0300

    Connection module refactoring

--- app/db_connection.py
+++ app/db_connection.py
@@ -2,8 +2,10 @@
 
 
 import os
+from typing import Generator
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 from contextlib import contextmanager
@@ -15,7 +17,7 @@ load_dotenv()
 Base = declarative_base()
 
 
-def get_engine():
+def get_engine() -> Engine:
     DB_USER = os.environ.get('DB_USER')
     DB_PASSWORD = os.environ.get('DB_PASSWORD')
     DB_HOST = os.environ.get('DB_HOST', 'localhost')
@@ -27,15 +29,24 @@ def get_engine():
 
     DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=20, pool_timeout=30)
+
     return engine
 
 
 @contextmanager
-def get_db_session():
+def get_db_session() -> Generator[Session, None, None]:
     engine = get_engine()
     Session = sessionmaker(bind=engine)
     session = Session()
     try:
         yield session
+    except Exception as e:
+        session.rollback()
+        raise
     finally:
         session.close()
+
+
+if __name__ == "__main__":
+    with get_db_session() as session:
+        pass
