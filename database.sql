6fa635e0161f1670fd31e8c4d9b1b1c6ac68bdde
himetik <himetik@tutamail.com>
2024-10-19 20:11:48 +0300

    Update sentences table

--- database.sql
+++ database.sql
@@ -6,6 +6,6 @@ DROP TABLE IF EXISTS sentences;
 
 CREATE TABLE sentences (
     id SERIAL PRIMARY KEY,
-    sentence TEXT NOT NULL,
+    sentence TEXT NOT NULL UNIQUE,
     timestamp TIMESTAMPTZ DEFAULT NOW()
 );
