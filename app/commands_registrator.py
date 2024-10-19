ffc1487be1edb81eddc47699c6deb390ca6be415
himetik <himetik@tutamail.com>
2024-10-19 21:07:20 +0300

    Command registration refactoring

--- app/commands_registrator.py
+++ app/commands_registrator.py
@@ -1,17 +1,21 @@
-"""Commands registration"""
-
-
 import click
-from .commands import num, random
+from typing import Any
+from .commands import num, random, word
 
 
 @click.group()
-def cli():
+def cli() -> None:
     pass
 
 
-cli.add_command(num)
-cli.add_command(random)
+def register_commands() -> None:
+    commands = [num, random, word]
+    for command in commands:
+        cli.add_command(command)
+
+
+register_commands()
+
 
 if __name__ == '__main__':
     cli()
